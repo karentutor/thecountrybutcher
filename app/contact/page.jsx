876da4e4c42ffaebe0aea1b5f5e6173d90945b61
yousefomar724 <yousefomar724@gmail.com>
2023-07-18 18:17:47 +0300

    our-story page - contact-us page done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,243 @@
+'use client'
+
+import Image from 'next/image'
+import { useForm } from 'react-hook-form'
+
 const Contact = () => {
-  return <div>Contact</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <div className='font-oswald bg-white'>
+      {/* Hero */}
+      <div className='h-screen w-full'>
+        <Image
+          src='/imgs/contact-hero.jpg'
+          width={0}
+          height={0}
+          sizes='100vw'
+          alt='big family'
+          className='w-full h-full object-cover'
+        />
+      </div>
+      {/* Contact Info. */}
+      <div className='flex flex-col gap-14 max-w-7xl mx-auto px-5 py-14 md:py-20'>
+        <h1 className='font-bold text-4xl md:text-5xl lg:text-6xl text-secondary'>
+          Lorem ipsum dolor sit.
+        </h1>
+        <div className='flex flex-col md:flex-row gap-10'>
+          {/* 1 */}
+          <div className='flex-1 flex flex-col gap-10'>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem - dolor sit amet - consectetur adipisicing elit.
+              </p>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <div className='text-secondary text-xl space-y-3'>
+                <p>
+                  Phone:{' '}
+                  <span className='text-primary-900'>432961427854312</span>
+                </p>
+                <p>
+                  Whatsapp:{' '}
+                  <span className='text-primary-900'>432961427854312</span>
+                </p>
+              </div>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis, unde!
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem - dolor sit amet - consectetur adipisicing elit.
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem - dolor sit amet - consectetur adipisicing elit.
+              </p>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+                fugit.
+              </p>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className='flex-1 flex flex-col gap-10'>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem - dolor sit amet - consectetur adipisicing elit.
+              </p>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+                excepturi temporibus magni debitis. Iusto minima culpa,
+                similique nihil nemo, dignissimos alias impedit harum
+                consequatur dolore soluta nesciunt animi ratione? Consequatur
+                quod quaerat ab cumque expedita quia modi ducimus omnis dolorum.
+              </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h2 className='text-3xl md:text-4xl text-secondary'>
+                Lorem, ipsum dolor.
+              </h2>
+              <p className='text-gray-700 text-xl font-thin'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                magni dicta, voluptate consequuntur dolores dolorem minima quos
+                nesciunt sequi, maxime soluta modi, quis et? Repudiandae quas
+                fugit explicabo voluptates dolor. Neque maxime aperiam vel
+                placeat deleniti, ipsa asperiores ullam voluptatibus ratione
+                consequuntur illo consequatur molestias numquam facilis dolorum
+                delectus quas?
+              </p>
+            </div>
+            <a
+              href='https://www.google.com/maps?ll=36.776807,-76.091684&z=15&t=m&hl=en&gl=EG&mapclient=embed&q=3640+Dam+Neck+Rd+%23216+Virginia+Beach,+VA+23453+USA'
+              target='_blank'
+              rel='noreferrer'
+              className='uppercase text-primary-900 underline text-xl'
+            >
+              get directions
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Form */}
+      <div className='bg-primary-200 py-14 md:py-20 px-5'>
+        <div className='max-w-7xl mx-auto flex flex-col gap-8'>
+          <h2 className='font-bold text-4xl md:text-5xl lg:text-6xl text-secondary uppercase'>
+            get in touch
+          </h2>
+          <p className='text-gray-700 text-lg'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+            molestiae architecto accusamus?
+          </p>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='flex flex-col gap-4 text-gray-700'
+          >
+            <div className='flex flex-col md:flex-row w-full gap-4'>
+              <div className='flex flex-col gap-1 w-full'>
+                <label htmlFor='f_name'>First Name</label>
+                <input
+                  type='text'
+                  {...register('f_name', {
+                    required: { value: true, message: 'required' },
+                  })}
+                  className={`w-full border outline-none p-3 rounded-lg bg-white ${
+                    errors.f_name ? 'border-red-500' : 'border-primary-900'
+                  }`}
+                />
+                {errors.f_name && (
+                  <span className='text-red-500'>{errors.f_name.message}</span>
+                )}
+              </div>
+              <div className='flex flex-col gap-1 w-full'>
+                <label htmlFor='l_name'>Last Name</label>
+                <input
+                  type='text'
+                  {...register('l_name', {
+                    required: { value: true, message: 'required' },
+                  })}
+                  className={`w-full border outline-none p-3 rounded-lg bg-white ${
+                    errors.l_name ? 'border-red-500' : 'border-primary-900'
+                  }`}
+                />
+                {errors.l_name && (
+                  <span className='text-red-500'>{errors.l_name.message}</span>
+                )}
+              </div>
+            </div>
+            <div className='flex flex-col md:flex-row w-full gap-4'>
+              <div className='flex flex-col gap-1 w-full'>
+                <label htmlFor='email'>Email</label>
+                <input
+                  type='email'
+                  {...register('email', {
+                    required: { value: true, message: 'required' },
+                  })}
+                  className={`w-full border outline-none p-3 rounded-lg bg-white ${
+                    errors.email ? 'border-red-500' : 'border-primary-900'
+                  }`}
+                />
+                {errors.email && (
+                  <span className='text-red-500'>{errors.email.message}</span>
+                )}
+              </div>
+              <div className='flex flex-col gap-1 w-full'>
+                <label htmlFor='phone'>Phone</label>
+                <input
+                  type='text'
+                  {...register('phone', {
+                    required: { value: true, message: 'required' },
+                  })}
+                  className={`w-full border outline-none p-3 rounded-lg bg-white ${
+                    errors.phone ? 'border-red-500' : 'border-primary-900'
+                  }`}
+                />
+                {errors.phone && (
+                  <span className='text-red-500'>{errors.phone.message}</span>
+                )}
+              </div>
+            </div>
+            <div className='flex flex-col gap-1 w-full'>
+              <label htmlFor='message'>Message</label>
+              <textarea
+                type='text'
+                rows={5}
+                {...register('message', {
+                  required: { value: true, message: 'required' },
+                })}
+                className={`w-full border outline-none p-3 rounded-lg bg-white ${
+                  errors.message ? 'border-red-500' : 'border-primary-900'
+                }`}
+              ></textarea>
+              {errors.message && (
+                <span className='text-red-500'>{errors.message.message}</span>
+              )}
+            </div>
+            <button className='bg-primary-900 px-12 py-4 mt-4 text-2xl tracking-widest text-white uppercase w-fit'>
+              submit form
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
--- app/our-story/page.jsx
+++ app/our-story/page.jsx
@@ -1,5 +1,176 @@
+import Image from 'next/image'
+
 const OurStory = () => {
-  return <div>OurStory</div>
+  return (
+    <div className='font-oswald'>
+      {/* Hero */}
+      <div className='h-screen w-full'>
+        <Image
+          src='/imgs/story-hero.jpg'
+          width={0}
+          height={0}
+          sizes='100vw'
+          alt='big family'
+          className='w-full h-full object-cover'
+        />
+      </div>
+      <div className='bg-secondary text-white'>
+        {/* Story */}
+        <div className='px-5 flex flex-col gap-4 md:gap-8 pt-14 md:pt-20 max-w-7xl mx-auto'>
+          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, at
+            cum
+          </h1>
+          <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
+            distinctio cupiditate quaerat est iusto veniam a esse fugit! Culpa
+            unde quis sint? Enim corporis praesentium distinctio error
+            consectetur quod fugit.
+          </p>
+          <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+            laudantium tempora quod quas quo voluptatem ipsam illum nesciunt et
+            nemo sed, assumenda placeat accusantium quasi facere quibusdam ad
+            ratione saepe. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Rem, molestias tempore! Autem. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Deserunt quibusdam minima velit veniam
+            sequi veritatis quae ullam dolorum, ut voluptatum!
+          </p>
+        </div>
+        {/* story 2 */}
+        <div className='px-5 flex flex-col md:flex-row gap-8 pt-14 md:pt-20 max-w-7xl mx-auto'>
+          {/* image */}
+          <div className='flex-1'>
+            <Image
+              src='/imgs/story2.jpg'
+              alt='small familly'
+              width={0}
+              height={0}
+              sizes='100vw'
+              className='w-full h-full object-cover'
+            />
+          </div>
+          {/* Content */}
+          <div className='flex-1 flex flex-col justify-center gap-6'>
+            <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eius
+              voluptate deleniti minus exercitationem odit ex qui atque
+              assumenda dolor rerum velit, officiis non eligendi enim
+              repudiandae sequi consectetur molestiae. Repellendus voluptatum
+              sint recusandae vel!
+            </p>
+            <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+              ratione porro dignissimos, dolorum fuga tempore corrupti ad
+              perspiciatis? Ipsa earum debitis qui quis mollitia at laudantium
+              nulla repellendus sit ullam et autem tenetur ut, quasi fuga
+              consequuntur adipisci cumque natus atque expedita. Obcaecati,
+              quibusdam adipisci!
+            </p>
+          </div>
+        </div>
+        {/* story 3 */}
+        <div className='px-5 flex flex-col md:flex-row gap-8 py-14 md:py-20 max-w-7xl mx-auto'>
+          {/* image */}
+          <div className='flex-1'>
+            <Image
+              src='/imgs/story3.jpg'
+              alt='small familly'
+              width={0}
+              height={0}
+              sizes='100vw'
+              className='w-full h-full object-cover'
+            />
+          </div>
+          {/* Content */}
+          <div className='flex-1 flex flex-col justify-center gap-6'>
+            <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eius
+              voluptate deleniti minus exercitationem odit ex qui atque
+              assumenda dolor rerum velit, officiis non eligendi enim
+              repudiandae sequi consectetur molestiae. Repellendus voluptatum
+              sint recusandae vel!
+            </p>
+            <p className='text-gray-200 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+              ratione porro dignissimos, dolorum fuga tempore corrupti ad
+              perspiciatis? Ipsa earum debitis qui quis mollitia at laudantium
+              nulla repellendus sit ullam et autem tenetur ut, quasi fuga
+              consequuntur adipisci cumque natus atque expedita. Obcaecati,
+              quibusdam adipisci!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='bg-white px-5'>
+        {/* Images */}
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto py-12 md:py-16'>
+          <Image
+            src='/imgs/meat1.jpg'
+            alt='meat1'
+            width={0}
+            height={0}
+            sizes='100vw'
+            className='w-full h-full object-cover'
+          />
+          <Image
+            src='/imgs/meat2.jpg'
+            alt='meat1'
+            width={0}
+            height={0}
+            sizes='100vw'
+            className='w-full h-full object-cover'
+          />
+          <Image
+            src='/imgs/meat3.jpg'
+            alt='meat1'
+            width={0}
+            height={0}
+            sizes='100vw'
+            className='w-full h-full object-cover'
+          />
+        </div>
+        {/* Cards */}
+        <div className='flex flex-col md:flex-row gap-12 max-w-7xl mx-auto py-12 md:py-16'>
+          {/* Card 1 */}
+          <div className='flex-1 flex flex-col gap-8 p-6'>
+            <h2 className='text-secondary text-3xl md:text-4xl lg:text-5xl'>
+              Lorem, ipsum dolor.
+            </h2>
+            <p className='text-gray-600 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              ipsa, esse dolorem voluptates dignissimos rem necessitatibus
+              fugiat reiciendis quasi dolor provident voluptatum. Animi,
+              inventore praesentium?
+            </p>
+            <p className='text-gray-600 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim,
+              quidem. Architecto facilis voluptates dolor? Sapiente a dolores
+              voluptatum in mollitia, molestias dolorem dicta. Officiis ad
+              magnam, impedit sapiente odio praesentium atque accusamus sequi
+              labore iure.
+            </p>
+          </div>
+          {/* Card 2 */}
+          <div className='flex-1 flex flex-col gap-8 bg-primary-200 p-6'>
+            <h2 className='text-secondary text-3xl md:text-4xl lg:text-5xl'>
+              Lorem ipsum dolor sit amet consectetur.
+            </h2>
+            <p className='text-gray-600 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              ipsa, esse dolorem voluptates dignissimos rem necessitatibus
+              fugiat reiciendis quasi dolor provident voluptatum. Animi,
+              inventore praesentium?
+            </p>
+            <p className='text-gray-600 text-xl md:text-2xl font-thin leading-relaxed md:leading-loose'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              voluptates dolor? Sapiente a dolores labore iure.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default OurStory
